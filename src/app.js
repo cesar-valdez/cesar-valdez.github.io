4f281584ec1fcf7eb677ef608418990a7fb17a4e
César Valdez <cesar.valdez1095@gmail.com>
2019-01-18 23:14:24 -0600

    add information component

--- src/app.js
+++ src/app.js
@@ -6,8 +6,9 @@ import 'evil-icons/assets/evil-icons.js';
 import './scss/app';
 
 import NetworkSocial from './components/networkSocial';
+import Information from './components/information';
 
-const App = () => <NetworkSocial />;
+const App = () => <React.Fragment><Information/><NetworkSocial/></React.Fragment>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
